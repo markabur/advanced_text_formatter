e9fa6ba7327c4931e62912cfec68d5f1c0eecc1e
Nhat Tran <thmnhat@gmail.com>
2013-06-01 16:40:23 +0700

    Port to 8.x

---
+++ lib/Drupal/advanced_text_formatter/Plugin/field/formatter/AdvancedTextFormatter.php
@@ -0,0 +1,289 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\advanced_text_formatter\Plugin\field\formatter\AdvancedTextFormatter.
+ */
+
+namespace Drupal\advanced_text_formatter\Plugin\field\formatter;
+
+use Drupal;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Utility\Token;
+use Drupal\field\Plugin\Type\Formatter\FormatterBase;
+use Drupal\filter\Plugin\Core\Entity\FilterFormat;
+
+/**
+ * Plugin implementation of the 'advanced_text_formatter' formatter.
+ *
+ * @Plugin(
+ *   id = "advanced_text",
+ *   module = "advanced_text_formatter",
+ *   label = @Translation("Advanced Text"),
+ *   field_types = {
+ *     "text",
+ *     "text_long",
+ *     "text_with_summary"
+ *   },
+ *   settings = {
+ *     "trim_length" = "600",
+ *     "ellipsis" = "1",
+ *     "word_boundary" = "1",
+ *     "token_replace" = "0",
+ *     "filter" = "input",
+ *     "format" = "plain_text",
+ *     "allowed_html" = "<a> <b> <br> <dd> <dl> <dt> <em> <i> <li> <ol> <p> <strong> <u> <ul>",
+ *     "autop" = "0"
+ *   }
+ * )
+ */
+class AdvancedTextFormatter extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, array &$form_state) {
+    $elid_trim   = drupal_html_id('advanced_text_formatter_trim');
+    $elid_filter = drupal_html_id('advanced_text_formatter_filter');
+
+    $element['trim_length'] = array(
+      '#id'               => $elid_trim,
+      '#type'             => 'number',
+      '#title'            => t('Trim length'),
+      '#description'      => t("Set this to 0 if you don't want to cut the text. Otherwise, input a positive integer."),
+      '#size'             => 10,
+      '#default_value'    => $this->getSetting('trim_length'),
+      '#required'         => TRUE,
+    );
+
+    $element['ellipsis'] = array(
+      '#type'          => 'checkbox',
+      '#title'         => t('Ellipsis'),
+      '#description'   => t('If checked, a "..." will be added if a field was trimmed.'),
+      '#default_value' => $this->getSetting('ellipsis'),
+      '#states'        => array(
+        'visible' => array(
+          '#' . $elid_trim  => array('!value' => '0'),
+        ),
+      ),
+    );
+
+    $element['word_boundary'] = array(
+      '#type'          => 'checkbox',
+      '#title'         => t('Word Boundary'),
+      '#description'   => t('If checked, this field be trimmed only on a word boundary.'),
+      '#default_value' => $this->getSetting('word_boundary'),
+      '#states'        => array(
+        'visible' => array(
+          '#' . $elid_trim  => array('!value' => '0'),
+        ),
+      ),
+    );
+
+    $token_link = _advanced_text_formatter_browse_tokens($this->instance['entity_type']);
+
+    $element['token_replace'] = array(
+      '#type'          => 'checkbox',
+      '#description'   => t('Replace text pattern. e.g %node-title-token or %node-author-name-token, by token values.', array(
+                            '%node-title-token'       => '[node:title]',
+                            '%node-author-name-token' => '[node:author:name]',
+                          )) . ' ' /*. $token_link*/,
+      '#title'         => t('Token Replace'),
+      '#default_value' => $this->getSetting('token_replace'),
+    );
+
+    $element['filter'] = array(
+      '#id'      => $elid_filter,
+      '#title'   => t('Filter'),
+      '#type'    => 'select',
+      '#options' => array(
+        'none'   => t('None'),
+        'input'      => t('Selected Text Format'),
+        'php'        => t('Limit allowed HTML tags'),
+        'drupal'     => t('Drupal'),
+      ),
+      '#default_value' => $this->getSetting('filter'),
+    );
+
+    $element['format'] = array(
+      '#title'         => t('Format'),
+      '#type'          => 'select',
+      '#options'       => array(),
+      '#default_value' => $this->getSetting('format'),
+      '#states'        => array(
+        'visible' => array(
+          '#' . $elid_filter  => array('value' => 'drupal'),
+        ),
+      ),
+    );
+
+    $formats = filter_formats();
+
+    foreach ($formats as $format_id => $format) {
+      $element['format']['#options'][$format_id] = $format->name;
+    }
+
+    $allowed_html = $this->getSetting('allowed_html');
+
+    if (empty($allowed_html)) {
+      $tags = '';
+    }
+    elseif (is_string($allowed_html)) {
+      $tags = $allowed_html;
+    }
+    else {
+      $tags = '<' . implode('> <', $allowed_html) . '>';
+    }
+
+    $element['allowed_html'] = array(
+      '#type'              => 'textfield',
+      '#title'             => t('Allowed HTML tags'),
+      '#description'       => t('See <a href="@link" target="_blank">filter_xss()</a> for more information', array(
+                                '@link' => 'http://api.drupal.org/api/drupal/includes%21common.inc/function/filter_xss/8',
+                              )),
+      '#default_value'     => $tags,
+      '#element_validate'  => array('_advanced_text_formatter_validate_allowed_html'),
+      '#states'            => array(
+        'visible' => array(
+          '#' . $elid_filter  => array('value' => 'php'),
+        ),
+      ),
+    );
+
+    $element['autop'] = array(
+      '#title'         => t('Converts line breaks into HTML (i.e. &lt;br&gt; and &lt;p&gt;) tags.'),
+      '#type'          => 'checkbox',
+      '#return_value'  => 1,
+      '#default_value' => $this->getSetting('autop'),
+      '#states'        => array(
+        'invisible' => array(
+          '#' . $elid_filter  => array('!value' => 'php'),
+        ),
+      ),
+    );
+
+    $element['br'] = array('#markup' => '<br/>');
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = array();
+    $yes     = t('Yes');
+    $no      = t('No');
+
+    if ($this->getSetting('trim_length') > 0) {
+      $summary[] = t('Trim length') . ': ' . $this->getSetting('trim_length');
+      $summary[] = t('Ellipsis') . ': ' . ($this->getSetting('ellipsis') ? $yes : $no);
+      $summary[] = t('Word Boundary') . ': ' . ($this->getSetting('word_boundary') ? $yes : $no);
+    }
+
+    $token_link = _advanced_text_formatter_browse_tokens($this->instance['entity_type']);
+    $summary[] = t('Token Replace') . ': ' . ($this->getSetting('token_replace') ? ($yes . '. ' . $token_link) : $no);
+
+    switch ($this->getSetting('filter')) {
+      case 'drupal':
+        $formats = filter_formats();
+        $format  = $this->getSetting('format');
+        $format  = isset($formats[$format]) ? $formats[$format]->name : t('Unknown');
+
+        $summary[] = t('Filter: @filter', array('@filter' => t('Drupal')));
+        $summary[] = t('Format: @format', array('@format' => $format));
+
+        break;
+
+      case 'php':
+        $text  = array();
+        $tags  = $this->getSetting('allowed_html');
+        $autop = $this->getSetting('autop');
+
+        if (is_array($tags)) {
+          $tags = '<' . implode('> <', $tags) . '>';
+        }
+
+        if (empty($tags)) {
+          $text[] = t('Remove all HTML tags.');
+        }
+        else {
+          $text[] = t('Limit allowed HTML tags: !tags.', array('!tags' => $tags));
+        }
+
+        if (!empty($autop)) {
+          $text[] = t('Convert line breaks into HTML.');
+        }
+
+        $summary[] = t('Filter: @filter', array('@filter' => implode(' ', $text)));
+
+        break;
+
+      case 'input':
+        $summary[] = t('Filter: @filter', array('@filter' => t('Selected Text Format')));
+
+        break;
+
+      default:
+        $summary[] = t('Filter: @filter', array('@filter' => t('None')));
+
+        break;
+    }
+
+    $summary = array_filter($summary);
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(EntityInterface $entity, $langcode, array $items) {
+    $elements   = array();
+    $token_data = array($entity->getType() => $entity);
+
+    foreach ($items as $delta => $item) {
+      $output = $item['value'];
+
+      if ($this->getSetting('token_replace')) {
+        $output = Drupal::token()->replace($output, $token_data);
+      }
+
+      switch ($this->getSetting('filter')) {
+        case 'drupal':
+          $output = check_markup($output, $this->getSetting('format'), $langcode);
+
+          break;
+
+        case 'php':
+          $output = filter_xss($output, $this->getSetting('allowed_html'));
+
+          if ($this->getSetting('autop')) {
+            $output = _filter_autop($output);
+          }
+
+          break;
+
+        case 'input':
+          $output = check_markup($output, $item['format'], $langcode);
+
+          break;
+      }
+
+      if ($this->getSetting('trim_length') > 0) {
+        $options  = array(
+          'word_boundary' => $this->getSetting('word_boundary'),
+          'max_length'    => $this->getSetting('trim_length'),
+          'ellipsis'      => $this->getSetting('ellipsis'),
+        );
+
+        $output = advanced_text_formatter_trim_text($output, $options);
+      }
+
+      $elements[$delta] = array('#markup' => $output);
+    }
+
+    return $elements;
+  }
+}
